b18ff8e32263c5396d1670f8ef4e550679565978
Andrey <andrey.28.06@mail.ru>
2025-10-19 17:05:18 +0000

    lala

--- frontend/src/components/MessageCard.jsx
+++ frontend/src/components/MessageCard.jsx
@@ -1,6 +1,41 @@
 import { deleteMessage, likeMessage, reportMessage } from "../api/api"
+import { useUserStore } from "../store/store"
+import { useMessageStore } from "../store/useMessageStore"
 
-const MessageCard = ({ content, createdAt, username, userId, id }) => {
+const MessageCard = ({ content, createdAt, username, userId, id, likes, likedBy }) => {
+    const loggedUserId = useUserStore((state) => state.jwt?.userId)
+    const { getMessages } = useMessageStore()
+
+    
+
+    const handleDelete = async () => {
+        try {
+         await deleteMessage(id)
+         await getMessages()
+        } catch (error) {
+            console.error(error)
+    }
+}
+
+const handleReport = async () => {
+    try {
+        await reportMessage(id)
+        await getMessages()
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const handleLike = async () => {
+    try {
+    await likeMessage(id)
+    await getMessages()
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
     return (
         <div className="message-card">
             <div className="message-content">{content}</div>
@@ -10,32 +45,33 @@ const MessageCard = ({ content, createdAt, username, userId, id }) => {
             </div>
             <div className="message-actions">
                 <button
-                    onClick={() => likeMessage(id)}
+                    onClick={handleLike}
                     className="action-button"
                 >
-                    <span>❤️🤍</span>
-                    <span>0</span>
+                    <span>{likedBy.includes(loggedUserId) ? "❤️" : "🤍"}</span>
+                    <span>{likes}</span>
                 </button>
             </div>
             <div className="message-actions">
                 <button
-                    onClick={() => reportMessage(id)}
+                    onClick={handleReport}
                     className="action-button"
                 >
                     <span>🚩</span>
                     <span>Пожаловаться</span>
                 </button>
             </div>
+            {loggedUserId == userId && (
             <div className="message-actions">
                 <button
-                    onClick={() => deleteMessage(id)}
-                    className="action-button delete"
-                >
+                    onClick={handleDelete}
+                    className="action-button delete">
                     <span>🗑️</span>
                     <span>Удалить</span>
                 </button>
             </div>
-        </div>
+            )}
+            </div>
     )
 }
 
